1843f7f3b53473529e4b5904eea637b598b862ed
deepakbaghel18 <db691612@gmail.com>
2026-08-11 00:49:00 +0530

    Add delete order route

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -5,12 +5,19 @@ const {
   createOrder,
   getOrders,
   updateOrderStatus,
+  deleteOrder,
 } = require("../controllers/orderController");
 
+// Create Order
 router.post("/", createOrder);
 
+// Get All Orders
 router.get("/", getOrders);
 
+// Mark Order Delivered
 router.put("/:id", updateOrderStatus);
+
+// Delete Order
+router.delete("/:id", deleteOrder);
 
 module.exports = router;
